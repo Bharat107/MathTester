d218989cea212e1016b3a7b4b3add2e996a7f71b
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-02-08 08:13:38 -0500

    Added new number variables and initialized it

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,10 +3,16 @@ public class MathTester {
     public static void main (String[] args){
         String firstWord;
         String secondWord;
+        int firstNumber, secondNumber;
+
+
 
         firstWord = "dog";
-        secondWord = "house;"
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
 
+        System.out.println(firstWord+secondWord);
 
 
 
